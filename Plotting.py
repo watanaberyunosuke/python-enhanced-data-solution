36182249941745f5ae153f8a003fb72e795206fc
Watanabe Ryunosuke <harryzhanchaoxi@gmail.com>
2018-06-30 16:11:56 +1000

    Add files via upload

---
+++ Plotting.py
@@ -0,0 +1,19 @@
+# Importing packages
+
+import matplotlib.pyplot as plt
+# Unsorted # import matplotlib.colors as colors
+import numpy as np
+
+# Datasetting
+
+x = np.linspace(-1, 1, 256)
+y = np.sin(x)
+
+# Formating
+
+
+
+# Ploting and force show(if not in spyder3)
+
+plt.plot(x, y)
+plt.show()
---
+++ name.py
@@ -0,0 +1,17 @@
+import re
+
+print("Who are you?")
+
+
+#Input control
+
+x=input("You are?")
+guess=str(x)
+
+while x == 'Harry':
+    print("Welcome Harry")
+    exit()
+
+else:
+    print("fuck off")
+    exit()

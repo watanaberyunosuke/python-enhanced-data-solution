e1cf9d11210a1d41592d82a12fd5429658943763
Harry Watson <harryzhanchaoxi@gmail.com>
2018-09-25 21:48:36 +1000

    Updated when mode not found

---
+++ Data Analysis Support System.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mp
+import statistics as stat
+from itertools import groupby
+
+# Read Excel by Using pandas, File Name and Sheets Name to be filled
+file = '/User/HarryZhan/Desktop/Test.xlsx'
+xl = pd.ExcelFile(file)
+print(xl.sheet_names)
+df1 = xl.parse('sheet1')
+#Sorting
+print("Sorted List:",sorted(data))
+# Output
+print("Minimum:",min(data))
+print("Maximum:",max(data))
+print("Sum:",sum(data))
+print("Mean:",stat.mean(data))
+print("Median",stat.median(data))
+# Mode
+Sortlist = data
+# when the list is sorted, groupby groups by consecutive elements which are similar
+for z, y in groupby(sorted(Sortlist)):
+    #  list(y) returns all the occurences of item x
+    if len(list(y)) > 1:
+        print("Mode:", z)
+    else:
+        print("No Mode")
+# Continue
+print("Standard Deviation:",stat.stdev(data))
+print("Variance:",stat.variance(data))
+print("Range:",max(data) - min(data))
--- Descriptive Statistic.py
+++ Descriptive Statistic.py
@@ -4,7 +4,7 @@ import matplotlib as mp
 import statistics as stat
 from itertools import groupby
 # Input Array to fill
-x = list([3, 4, 2, 5, 1, 6, 7, 1, 1])
+x = list([3, 4, 2, 5, 1, 6, 7, 8, 0])
 print("Input:",x)
 #Sorting
 print("Sorted List:",sorted(x))
@@ -21,6 +21,8 @@ for z, y in groupby(sorted(Sortlist)):
     #  list(y) returns all the occurences of item x
     if len(list(y)) > 1:
         print("Mode:", z)
+    else:
+        (print("Mode not Find"))
 # Continue
 print("Standard Deviation:",stat.stdev(x))
 print("Variance:",stat.variance(x))
